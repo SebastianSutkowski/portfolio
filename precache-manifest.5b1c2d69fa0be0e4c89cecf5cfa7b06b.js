a713ab79e8f16fc9b7903324eb712cdc6848c159
Sebastian Sutkowski <sebastian.sutkowski1@gmail.com>
2020-05-22 16:05:29 +0200

    Updates

---
+++ precache-manifest.5b1c2d69fa0be0e4c89cecf5cfa7b06b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c403c362c1838efb31b289721ef22b4d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4b93e414f883cfe6fc3d",
+    "url": "/portfolio/static/css/main.c48360a9.chunk.css"
+  },
+  {
+    "revision": "85607ad14f6caa4edb6f",
+    "url": "/portfolio/static/js/2.adbbe165.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.adbbe165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b93e414f883cfe6fc3d",
+    "url": "/portfolio/static/js/main.68340a32.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "e225b1469550e3a0f75885574d390b11",
+    "url": "/portfolio/static/media/dog.e225b146.jpg"
+  },
+  {
+    "revision": "b6df5606abc702c71b9a7c848c4c1326",
+    "url": "/portfolio/static/media/medyk.b6df5606.PNG"
+  },
+  {
+    "revision": "2c247d430a1ab07278ec363b188b517f",
+    "url": "/portfolio/static/media/portfolio.2c247d43.PNG"
+  },
+  {
+    "revision": "16cd63f80e805c573d275dff9d13fd91",
+    "url": "/portfolio/static/media/svgAbout.16cd63f8.svg"
+  },
+  {
+    "revision": "496742211a2f50a38bd4d7b9d728e540",
+    "url": "/portfolio/static/media/svgMain.49674221.svg"
+  },
+  {
+    "revision": "b8f536bce4ab15ae83926078dd3c5710",
+    "url": "/portfolio/static/media/svgSkills.b8f536bc.svg"
+  }
+]);
